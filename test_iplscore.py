0c13b6efe5e89dc5921e80cf5e53bfd20bf7aeb9
Fülöp Koppány-Bence <59284025+fkoppanybence@users.noreply.github.com>
2023-01-18 18:06:47 +0200

    tests

---
+++ test_iplscore.py
@@ -0,0 +1,8 @@
+from iplscore import home
+from flask import render_template, Flask
+app = Flask(__name__)
+
+@app.route('/')
+def test_home():
+    with app.app_context():
+        assert home() == render_template('home.html',val='')
